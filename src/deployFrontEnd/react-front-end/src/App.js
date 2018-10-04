2ec85feee53c37bd1d863743fd0c7e1b10df793c
Jun Marcel Fritz <jun.fritz@gmail.com>
2018-10-03 17:38:08 -0700

    add routes and styles to app.js and .css

--- src/deployFrontEnd/react-front-end/src/App.js
+++ src/deployFrontEnd/react-front-end/src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Content from './components/Content';
+import About from './components/About';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Stackery React App
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HashRouter>
+          <div className="container">
+            <ul className="nav">
+              <li><NavLink exact to="/">Home</NavLink></li>
+              <li><NavLink to="/content">Content</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+            </ul>
+            <div className="pages">
+              <Route exact path="/" component={Home}/>
+              <Route path="/content" component={Content}/>
+              <Route path="/about" component={About}/>
+            </div>
+          </div>
+        </HashRouter>
       </div>
     );
   }
